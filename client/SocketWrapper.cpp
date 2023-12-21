155660269496324fc52f5387b8c8d8d8f3aad985
chriskontsis <cak0927@gmail.com>
2023-12-21 12:52:43 -0800

    cleaning socket wrapper comments

--- client/SocketWrapper.cpp
+++ client/SocketWrapper.cpp
@@ -1,10 +1,12 @@
 #include "SocketWrapper.hpp"
 
-boost::asio::ip::tcp::socket &SocketWrapper::getSocket()
-{
-    return socket_;
-}
 
+
+SocketWrapper::SocketWrapper(boost::asio::io_context &io_context, const std::string& host, int port)
+        : socket_(io_context), endpoint_(boost::asio::ip::address::from_string(host), port)
+    {
+        connect();
+    }
 void SocketWrapper::writeToSocket(const std::string_view &data)
 {
         boost::system::error_code error;
@@ -13,3 +15,11 @@ void SocketWrapper::writeToSocket(const std::string_view &data)
             std::cerr << "Error writing to socket: " << error.message() << std::endl;
         }
 }
+
+void SocketWrapper::connect() {
+        boost::system::error_code error;
+        socket_.connect(endpoint_, error);
+        if (error) {
+            std::cerr << "Error connecting to server: " << error.message() << std::endl;
+        }
+    }
--- client/SocketWrapper.hpp
+++ client/SocketWrapper.hpp
@@ -1,26 +1,3 @@
-// #ifndef SOCKET_WRAPPER_HPP
-// #define SOCKET_WRAPPER_HPP
-
-// #include <boost/asio.hpp>
-// #include <string>
-// #include <iostream>
-// #include <string_view>
-
-// class SocketWrapper
-// {
-// public:
-//     SocketWrapper(boost::asio::io_context &io_context, int port)
-//         : socket_(io_context) {}
-
-//     boost::asio::ip::tcp::socket &getSocket();
-//     void writeToSocket(const std::string_view &data);
-
-// private:
-//     boost::asio::ip::tcp::acceptor acceptor_;
-//     boost::asio::ip::tcp::socket socket_;
-// };
-
-// #endif
 #ifndef SOCKET_WRAPPER_HPP
 #define SOCKET_WRAPPER_HPP
 
@@ -32,26 +9,13 @@
 class SocketWrapper
 {
 public:
-    SocketWrapper(boost::asio::io_context &io_context, const std::string& host, int port)
-        : socket_(io_context), endpoint_(boost::asio::ip::address::from_string(host), port)
-    {
-        connect();
-    }
-
-    boost::asio::ip::tcp::socket &getSocket();
+    SocketWrapper(boost::asio::io_context &io_context, const std::string& host, int port);
     void writeToSocket(const std::string_view &data);
 
 private:
     boost::asio::ip::tcp::endpoint endpoint_;
     boost::asio::ip::tcp::socket socket_;
-
-    void connect() {
-        boost::system::error_code error;
-        socket_.connect(endpoint_, error);
-        if (error) {
-            std::cerr << "Error connecting to server: " << error.message() << std::endl;
-        }
-    }
+    void connect();
 };
 
 #endif
