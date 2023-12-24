bd1699f523c05654db8e3f29dd7b3e157cf2ce18
chriskontsis <cak0927@gmail.com>
2023-12-24 15:20:40 -0800

    styling ob

--- server/OrderBookWidget.py
+++ server/OrderBookWidget.py
@@ -1,88 +1,8 @@
-from PyQt5.QtWidgets import  QWidget, QHBoxLayout, QListWidget, QListWidgetItem
-from PyQt5.QtCore import  QThread
-from collections import OrderedDict
-from SocketThread import SocketThread
-
-
-# class OrderBookVisualizer(QWidget):
-#     def __init__(self):
-#         super().__init__()
-#         self.order_book_data = {}
-#         self.initUI()
-#         self.start_socket_thread()
-
-#     def initUI(self):
-#         layout = QHBoxLayout()
-
-#         # Create QListWidget for equities
-#         self.equity_list = QListWidget()
-#         self.equity_list.itemClicked.connect(self.on_equity_clicked)
-
-#         layout.addWidget(self.equity_list)
-
-#         # Create QListWidgets for buy and sell sides
-#         self.buy_list = QListWidget()
-#         self.sell_list = QListWidget()
-
-#         layout.addWidget(self.buy_list)
-#         layout.addWidget(self.sell_list)
-
-#         self.setLayout(layout)
-#         self.setWindowTitle('Order Book')
-
-#     def on_equity_clicked(self, item):
-#         ticker = item.text()
-#         buy_data = self.order_book_data[ticker]['buy']
-#         sell_data = self.order_book_data[ticker]['sell']
-
-#         # Clear previous data from buy and sell lists
-#         self.buy_list.clear()
-#         self.sell_list.clear()
-
-#         # Populate buy list
-#         sorted_buy_data = OrderedDict(sorted(buy_data.items(), key=lambda x: x[0], reverse=True))
-#         for price, quantity in sorted_buy_data.items():
-#             self.buy_list.addItem(f"Buy - Price: {price}, Quantity: {quantity}")
-
-#         # Populate sell list
-#         for price, quantity in sell_data.items():
-#             self.sell_list.addItem(f"Sell - Price: {price}, Quantity: {quantity}")
-
-#     def update_order_book(self, new_data):
-#         for line in new_data.split('#'):
-#             if line:
-#                 parts = line.split(',')
-#                 if len(parts) == 4:
-#                     action, ticker, price, quantity = parts
-#                     print(parts)
-#                     if ticker not in self.order_book_data:
-#                         self.order_book_data[ticker] = {'buy': {}, 'sell': {}}
-#                         item = QListWidgetItem(ticker)
-#                         self.equity_list.addItem(item)
-#                     if action.strip() == 'BUY':
-#                         self.order_book_data[ticker]['buy'][float(price)] = int(quantity)
-#                     elif action.strip() == 'SELL':
-#                         self.order_book_data[ticker]['sell'][float(price)] = int(quantity)
-
-#                     # Update the equity item text if it already exists
-#                     for index in range(self.equity_list.count()):
-#                         item = self.equity_list.item(index)
-#                         if item.text() == ticker:
-#                             item.setText(ticker)
-
-#     def start_socket_thread(self):
-#         self.socket_thread = SocketThread('127.0.0.1', 8080)
-#         #update order book is binded to the data recieved singal, everytime data comes in func
-#         self.socket_thread.data_received.connect(self.update_order_book)
-#         self.socket_qthread = QThread()
-#         self.socket_thread.moveToThread(self.socket_qthread)
-#         self.socket_qthread.started.connect(self.socket_thread.run)
-#         self.socket_qthread.start()
-
-from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QTableWidget, QTableWidgetItem, QVBoxLayout, QListWidget
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QTableWidget, QTableWidgetItem, QListWidgetItem, QListWidget
+from PyQt5.QtCore import QThread
 from SocketThread import SocketThread
 from collections import OrderedDict
+from PyQt5.QtGui import QColor
 
 class OrderBookVisualizer(QWidget):
     def __init__(self):
@@ -94,14 +14,19 @@ class OrderBookVisualizer(QWidget):
     def initUI(self):
         layout = QHBoxLayout()
 
-        # Create QListWidget for equities
         self.equity_list = QListWidget()
         self.equity_list.itemClicked.connect(self.on_equity_clicked)
+        self.equity_list.setStyleSheet("background-color: white; color: black;") 
 
-        # Create QTableWidget for order book
         self.order_table = QTableWidget()
         self.order_table.setColumnCount(3)
-        self.order_table.setHorizontalHeaderLabels(["Buy Volume", "Price", "Sell Volume"])
+        self.order_table.setHorizontalHeaderLabels(["Bid Volume", "Price", "Ask Volume"])
+        self.order_table.setStyleSheet(
+            "background-color: white; color: black; border: 1px solid black"
+        )  
+
+        header = self.order_table.horizontalHeader()
+        header.setStyleSheet("background-color: navy; color: white; border: 2px solid black;") 
 
         layout.addWidget(self.equity_list)
         layout.addWidget(self.order_table)
@@ -112,7 +37,7 @@ class OrderBookVisualizer(QWidget):
     def on_equity_clicked(self, item):
         ticker = item.text()
         print(self.order_book_data[ticker])
-        
+
         if ticker in self.order_book_data:
             self.order_table.clearContents()
 
@@ -120,21 +45,36 @@ class OrderBookVisualizer(QWidget):
             buy_data = data['buy']
             sell_data = data['sell']
 
+            buys = OrderedDict(sorted(buy_data.items(), key=lambda t: t[0]))
+            sells = OrderedDict(sorted(sell_data.items(), key=lambda t: t[0], reverse=True))
+
             rows = len(buy_data) + len(sell_data)
             self.order_table.setRowCount(rows)
 
-            row = 0
-            for _, (price, sell_volume) in enumerate(sell_data.items()):
-                self.order_table.setItem(row, 0, QTableWidgetItem(str(buy_data.get(price, ''))))                
-                self.order_table.setItem(row, 1, QTableWidgetItem(str(price)))
-                self.order_table.setItem(row, 2, QTableWidgetItem(str(sell_volume)))
-                row+=1
+            column_backgrounds = [
+                QColor("#DFF2FF"),  
+                QColor("#FFFFFF"),  
+                QColor("#F9CCCC")   
+            ]
 
-            for (price, buy_volume) in buy_data.items():
-                self.order_table.setItem(row, 0, QTableWidgetItem(str(buy_volume)))
-                self.order_table.setItem(row, 1, QTableWidgetItem(str(price)))
-                self.order_table.setItem(row, 2, QTableWidgetItem(str(sell_data.get(price, ''))))
-                row+=1
+            row = 0
+            for _, (price, sell_volume) in enumerate(sells.items()):
+                if not sell_volume:
+                    continue
+                for col, cell_value in enumerate([buy_data.get(price, ''), price, sell_volume]):
+                    item = QTableWidgetItem(str(cell_value))
+                    item.setBackground(column_backgrounds[col])
+                    self.order_table.setItem(row, col, item)
+                row += 1
+
+            for (price, buy_volume) in buys.items():
+                if not buy_volume:
+                    continue
+                for col, cell_value in enumerate([buy_volume, price, sell_data.get(price, '')]):
+                    item = QTableWidgetItem(str(cell_value))
+                    item.setBackground(column_backgrounds[col])
+                    self.order_table.setItem(row, col, item)
+                row += 1
 
 
     def update_equity_list(self):
