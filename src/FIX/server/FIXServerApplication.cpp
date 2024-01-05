2987aa09229a1829f6d015b685b47e9df888b0d9
chriskontsis <cak0927@gmail.com>
2024-01-05 15:40:20 -0800

    figuring out the server side of the FIX system

---
+++ src/FIX/server/FIXServerApplication.cpp
@@ -0,0 +1,14 @@
+#include "FIXServerApplication.hpp"
+
+void FIXServerApplication::onMessage(const FIX42::NewOrderSingle&, const FIX::SessionID& )
+{
+    FIX::Symbol symbol;
+    FIX::Side side;
+    FIX::OrdType orderType;
+    FIX::OrderQty orderQty;
+    FIX::Price price;
+    FIX::ClOrdID clOrdID;
+    FIX::Account account;
+
+    
+}
---
+++ src/FIX/server/FIXServerApplication.hpp
@@ -0,0 +1,36 @@
+#ifndef FIX_SERVER_APPLICATION
+#define FIX_SERVER_APPLICATION
+
+#include "quickfix/Application.h"
+#include "quickfix/MessageCracker.h"
+#include "quickfix/Values.h"
+#include "quickfix/Utility.h"
+#include "quickfix/Mutex.h"
+#include "quickfix/fix42/NewOrderSingle.h"
+
+#include <cstdint>
+
+class FIXServerApplication : public FIX::Application, public FIX::MessageCracker
+{
+private:
+    u_int64_t orderId;
+
+public:
+    FIXServerApplication() : orderId(0) {}
+
+    // Application overloads
+    void onCreate(const FIX::SessionID &) {}
+    void onLogon(const FIX::SessionID &sessionID) {}
+    void onLogout(const FIX::SessionID &sessionID) {}
+    void toAdmin(FIX::Message &, const FIX::SessionID &) {}
+    void toApp(FIX::Message &, const FIX::SessionID &)
+        EXCEPT(FIX::DoNotSend) {}
+    void fromAdmin(const FIX::Message &, const FIX::SessionID &)
+        EXCEPT(FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::RejectLogon) {}
+    void fromApp(const FIX::Message &message, const FIX::SessionID &sessionID)
+        EXCEPT(FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::UnsupportedMessageType) {}
+
+    void onMessage( const FIX42::NewOrderSingle&, const FIX::SessionID& );
+};
+
+#endif;
