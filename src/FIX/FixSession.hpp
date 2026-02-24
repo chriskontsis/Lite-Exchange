838f27be35a251961227ad3a6f9643e8230ce9ae
chriskontsis <cak0927@gmail.com>
2026-02-24 11:29:30 -0800

    Fix Session able to read and write to socket

--- src/FIX/FixSession.hpp
+++ src/FIX/FixSession.hpp
@@ -7,17 +7,45 @@
 #include "FixMessage.hpp"
 #include "../engine/EngineDispatcher.hpp"
 
-namespace fix {
+namespace fix
+{
     class FixSession : public std::enable_shared_from_this<FixSession>
     {
-        private:
-            boost::asio::ip::tcp::socket socket_;
-            EngineDispatcher&            dispatcher;
-            static constexpr int         BUFFER_SIZE = 1024;
-            char                         data_[BUFFER_SIZE];
+    private:
+        boost::asio::ip::tcp::socket socket_;
+        EngineDispatcher &dispatcher_;
+        static constexpr int BUFFER_SIZE = 1024;
+        char data_[BUFFER_SIZE];
+
+    public:
+        FixSession(boost::asio::ip::tcp::socket socket, EngineDispatcher &dispatcher)
+            : socket_(std::move(socket)), dispatcher_(dispatcher) {}
+
+        void doRead()
+        {
+            auto self = shared_from_this();
+            socket_.async_read_some(
+                boost::asio::buffer(data_, BUFFER_SIZE),
+                [this, self](boost::system::error_code ec, std::size_t length)
+                {
+                    if (ec)
+                    {
+                        std::cout << "Client disconnected\n";
+                        return;
+                    }
+                    auto req = FixMessage::parse(std::string_view(data_, length));
+                    if (req.type != MsgType::UNKNOWN)
+                        dispatcher_.route(req);
+                    doRead();
+                });
+        }
+
+        void start()
+        {
+            doRead();
+        }
     };
 
 } // namespace fix
-
 
 #endif
