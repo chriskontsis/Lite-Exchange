d6b091b40395ed81b4f49fd25d59459d542d4970
chriskontsis <cak0927@gmail.com>
2026-02-25 12:19:56 -0800

    Fixing Server for sessions between client

--- src/server/FixServer.hpp
+++ src/server/FixServer.hpp
@@ -1,19 +1,36 @@
-#ifndef FIX_SERVER_H
-#define FIX_SERVER_H
+#pragma once
 
-#include <boost/asio.hpp>
+#include <memory>
+#include <vector>
 #include "ServerBase.hpp"
-
-
-
-namespace fix {
-class FixServer : public ServerBase {
+#include "../fix/FixSession.hpp"
+namespace fix
+{
+    class FixServer : public ServerBase<FixServer>
+    {
     public:
-        FixServer(boost::asio::io_context& io_context, short port)
-            : ServerBase(io_context, port) {}
+        FixServer(boost::asio::io_context &io_context, short port, EngineDispatcher &dispatcher)
+            : ServerBase(io_context, port), dispatcher_(dispatcher) {}
 
-};
+        void onNewConnection(boost::asio::ip::tcp::socket socket)
+        {
+            auto session = std::make_shared<FixSession>(std::move(socket), dispatcher_);
+            sessions_.emplace_back(session);
+            pruneSessions();
+            session->start();
+        }
 
-} // end namespace fix
+    private:
+        void pruneSessions()
+        {
+            sessions_.erase(std::remove_if(sessions_.begin(), sessions_.end(), 
+            [](const std::weak_ptr<FixSession> &s) 
+            {
+                return s.expired();
+            }), sessions_.end());
+        }
 
-#endif
+        EngineDispatcher &dispatcher_;
+        std::vector<std::weak_ptr<FixSession>> sessions_;
+    };
+} // namespace fix
--- src/server/serverMain.cpp
+++ src/server/serverMain.cpp
@@ -1,9 +1,14 @@
+#include <iostream>
+#include <boost/asio.hpp>
 #include "FixServer.hpp"
-using namespace fix;
+#include "../engine/EngineDispatcher.hpp"
 
-int main() {
+int main() 
+{
+    fix::EngineDispatcher dispatcher;
     boost::asio::io_context io_context;
-    FixServer server(io_context, 12345);
-    std::thread serverThread([&]() {io_context.run(); });
-    serverThread.join();
+    fix::FixServer server(io_context, 12345, dispatcher);
+    std::cout << "Exchange listening on port 12345/n";
+    io_context.run();
+    return 0;
 }
