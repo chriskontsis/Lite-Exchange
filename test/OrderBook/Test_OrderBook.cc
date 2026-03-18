55b3ab5df91ca43444015f8cefb8be6d69af9d43
chriskontsis <cak0927@gmail.com>
2026-03-18 11:44:34 -0700

    adding orderbook tests in gtest

---
+++ test/OrderBook/Test_OrderBook.cc
@@ -0,0 +1,99 @@
+#include <gtest/gtest.h>
+#include "../../src/matching-engine/OrderBook.hpp"
+
+using namespace LOB;
+
+class OrderBookTest : public ::testing::Test
+{
+    protected:
+        LimitOrderBook book;
+};
+
+TEST_F(OrderBookTest, LimitBuyRestsOnBook)
+{
+    book.limit(Side::BUY, 1, 10, 100);
+    EXPECT_EQ(book.bestBid(), 100);
+    EXPECT_EQ(book.volumeAt(Side::BUY, 100), 10);
+    EXPECT_TRUE(book.hasOrder(1));
+}
+
+TEST_F(OrderBookTest, LimitSellRestsOnBook)
+{
+    book.limit(Side::SELL, 1, 10, 200);
+    EXPECT_EQ(book.bestAsk(), 200);
+    EXPECT_EQ(book.volumeAt(Side::SELL, 200), 10);
+    EXPECT_TRUE(book.hasOrder(1));
+}
+
+TEST_F(OrderBookTest, LimitSellMatchesBid)
+{
+    book.limit(Side::BUY, 1, 10, 100);
+    book.limit(Side::SELL, 2, 10, 100);
+    EXPECT_EQ(book.bestBid(), 0);
+    EXPECT_EQ(book.bestAsk(), 0);
+    EXPECT_FALSE(book.hasOrder(1));
+    EXPECT_FALSE(book.hasOrder(2));
+}
+
+TEST_F(OrderBookTest, LimitPartialFill)
+{
+    book.limit(Side::BUY, 1, 10, 100);
+    book.limit(Side::SELL, 2, 5, 100);
+    EXPECT_EQ(book.bestBid(), 100);
+    EXPECT_EQ(book.volumeAt(Side::BUY, 100), 5);
+    EXPECT_TRUE(book.hasOrder(1));
+    EXPECT_FALSE(book.hasOrder(2));
+}
+
+TEST_F(OrderBookTest, MarkeBuyNoLiquidity)
+{
+    book.market(Side::BUY, 1, 10);
+    EXPECT_EQ(book.bestAsk(), 0);
+}
+
+TEST_F(OrderBookTest, MarketBuyFillsAsk)
+{
+    book.limit(Side::SELL, 1, 10, 100);
+    book.market(Side::BUY, 2, 10);
+    EXPECT_EQ(book.bestAsk(), 0);
+    EXPECT_FALSE(book.hasOrder(1));
+}
+
+
+TEST_F(OrderBookTest, MarketBuyPartialFill)
+{
+    book.limit(Side::SELL, 1, 10, 100);
+    book.market(Side::BUY, 2, 5);
+    EXPECT_EQ(book.bestAsk(), 100);
+    EXPECT_EQ(book.volumeAt(Side::SELL, 100), 5);
+    EXPECT_TRUE(book.hasOrder(1));
+}
+
+TEST_F(OrderBookTest, CancelRemovesOrder)
+{
+    book.limit(Side::BUY, 1, 10, 100);
+    book.cancel(1);
+    EXPECT_EQ(book.bestBid(), 0);
+    EXPECT_FALSE(book.hasOrder(1));
+}
+
+TEST_F(OrderBookTest, CancelNonExistentIsSafe)
+{
+    book.cancel(9999);
+}
+
+TEST_F(OrderBookTest, ReducePartial)
+{
+    book.limit(Side::BUY, 1, 10, 100);
+    book.reduce(1, 4);
+    EXPECT_EQ(book.volumeAt(Side::BUY, 100), 6);
+    EXPECT_TRUE(book.hasOrder(1));
+}
+
+TEST_F(OrderBookTest, ReduceToZeroCancels)
+{
+    book.limit(Side::BUY, 1, 10, 100);
+    book.reduce(1, 10);
+    EXPECT_EQ(book.bestBid(), 0);
+    EXPECT_FALSE(book.hasOrder(1));
+}
